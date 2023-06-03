4942a31809062b5fd4ab078a414936eddbba6e1f
Mariya Goncharenko <114483796+mariya-goncharenko@users.noreply.github.com>
2023-06-03 15:04:45 +0300

    Исправление ошибок 1.1

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -11,7 +11,6 @@ export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
     method: "POST",
     headers: {
-      Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
